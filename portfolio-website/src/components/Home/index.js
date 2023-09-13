8bb97e500fa971c477be311be6f3ac5fe275bc27
Sabin Budhathoki <budhathokisabin303@gmail.com>
2023-09-13 17:13:38 +0800

    header and home components added

---
+++ portfolio-website/src/components/Home/index.js
@@ -0,0 +1,17 @@
+// Home/index.js
+
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>
+        This is where you can showcase your skills, projects, and more. Customize this
+        component to display your content.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
